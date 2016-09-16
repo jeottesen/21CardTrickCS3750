963312b1b7906bd922ae297845fdf7d4cba1f59c
Geese <gisela.chodos@gmail.com>
2016-09-16 12:25:30 -0600

    Changed my Board to match master's

--- src/Board.java
+++ src/Board.java
@@ -13,7 +13,6 @@ public class Board extends JPanel {
 	// Since the UML Diagram shows these are a composition
 	// relationship they need to be final. They would be
 	// an Aggregation relationship without it.
-	// (thanks, that's helpful to point out --geese)
 	private final Column column1 = new Column();
 	private final Column column2 = new Column();
 	private final Column column3 = new Column();
@@ -21,8 +20,7 @@ public class Board extends JPanel {
 	private Dealer dealer;	
 
 	public Board() {
-		dealer = new Dealer();
-		dealer.setBoard(this);
+		
 		
 		setLayout(null);
 		
@@ -36,6 +34,9 @@ public class Board extends JPanel {
 			System.out.println("ERROR: " + e.getMessage());
 		}
 		
+		dealer = new Dealer();
+		dealer.setBoard(this);
+		
 		// Temporary deck for testing. Will be getting cards from dealer later
 		Deck deck = new Deck();
 		
@@ -43,20 +44,15 @@ public class Board extends JPanel {
 		column2.setLocation(Globals.COLUMN_TWO_LOCX, Globals.COLUMN_TWO_LOCY);
 		column3.setLocation(Globals.COLUMN_THREE_LOCX, Globals.COLUMN_THREE_LOCY);
 		
-		column1.setId(1);
+		/*column1.setId(1);
 		column2.setId(2);
-		column3.setId(3);
+		column3.setId(3);*/
 		
 		add(column1);
 		add(column2);
 		add(column3);
 		
-		// Will move to Dealers deal function
-		for (int i = 0; i < Globals.CARDS_PER_COLUMN; i++) {
-			addToColumn(1, deck.draw());
-			addToColumn(2, deck.draw());
-			addToColumn(3, deck.draw());
-		}
+		
 	}
 	
 	public void addToColumn(int columnId, Card card) {
@@ -80,9 +76,9 @@ public class Board extends JPanel {
 	 * I'm returning a reference to an object.  I'll take
 	 * anyone's input about this.  --geese
 	 */
-	public Dealer getDealer(){
-		return dealer;
-	}
+	//public Dealer getDealer(){
+		//return dealer;
+	//}
 	
 		
 	@Override
