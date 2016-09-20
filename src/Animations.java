a5beb99fa4801dd5dbe663f3a25bf5c405d68e84
Jeremy Ottesen <jeottesen@gmail.com>
2016-09-20 15:46:41 -0600

    Upload initial Animation class
    Upload the initial animation class and the library files for the
    Universal Tween Engine

---
+++ src/Animations.java
@@ -0,0 +1,145 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.Timer;
+
+import aurelienribon.tweenengine.BaseTween;
+import aurelienribon.tweenengine.Tween;
+import aurelienribon.tweenengine.TweenAccessor;
+import aurelienribon.tweenengine.TweenCallback;
+import aurelienribon.tweenengine.TweenManager;
+import aurelienribon.tweenengine.equations.Bounce;
+
+public class Animations {
+
+	public static void movePanel(JPanel panel, int newX, int newY) {
+		
+		final TweenManager tweenManager = new TweenManager();
+		// start the thread that plays the animation
+		Timer timer = new Timer(25, new ActionListener() {
+			private long time = System.currentTimeMillis();
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				System.out.println((System.currentTimeMillis() - time));
+				// Passes the change in time to the tweenManager
+				// to correctly calculate the new location
+				tweenManager.update((System.currentTimeMillis() - time) / 1000.0f);
+			}
+			
+		});
+
+		Tween.registerAccessor(JPanel.class, new PanelAccessor());
+
+		// Pass it the object to animate the direction to animate it and the
+		// duration of the animation in milliseconds
+		Tween.to(panel, PanelAccessor.POS_XY, 50f)
+				// Start from the exact middle of the screen
+				.target(newX, newY).ease(Bounce.OUT).start(tweenManager)
+				// Stop the timer when the animation is complete
+				.setCallbackTriggers(TweenCallback.COMPLETE)
+				.setCallback(new TweenCallback() {
+					@Override
+					public void onEvent(int type, BaseTween<?> source) {
+						timer.stop();
+					}
+				});
+
+		
+		timer.start();
+
+	}
+
+	/**
+	 * Test and demonstration.
+	 */
+	public static void main(String[] args) {
+
+		JFrame frame = new JFrame();
+		frame.setLayout(null);
+		Card card = new Card(CardValues.ACE, CardSuits.SPADES);
+
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(Globals.FRAME_WI, Globals.FRAME_HI);
+		card.setLocation((Globals.FRAME_WI / 2) - (Globals.CARD_WI / 2), -1 * Globals.CARD_HI);
+		frame.add(card);
+		frame.setVisible(true);
+
+		Animations.movePanel(card, Globals.COLUMN_ONE_LOCX, Globals.COLUMN_ONE_LOCY);
+
+	}
+}
+
+// Used to tell Universal Tween Engine how to get and set the values of a JPanel
+// Copied from the demo on the projects website and modified to work for JPanels
+class PanelAccessor implements TweenAccessor<JPanel> {
+	
+	public static final int POS_XY = 1; // Position based on upper left corner of panel
+	public static final int CPOS_XY = 2; // Position based on center of panel
+	public static final int SCALE_XY = 3;
+	public static final int ROTATION = 4;
+	public static final int OPACITY = 5;
+	public static final int TINT = 6;
+
+	@Override
+	public int getValues(JPanel target, int tweenType, float[] returnValues) {
+		switch (tweenType) {
+			case POS_XY:
+				returnValues[0] = target.getX();
+				returnValues[1] = target.getY();
+				return 2;
+
+			case CPOS_XY:
+				returnValues[0] = target.getX() + target.getWidth()/2;
+				returnValues[1] = target.getY() + target.getHeight()/2;
+				return 2;
+
+			/*
+			case SCALE_XY:
+				returnValues[0] = target.getScaleX();
+				returnValues[1] = target.getScaleY();
+				return 2;
+
+			case ROTATION: returnValues[0] = target.getRotation(); return 1;
+			case OPACITY: returnValues[0] = target.getColor().a; return 1;
+
+			case TINT:
+				returnValues[0] = target.getColor().r;
+				returnValues[1] = target.getColor().g;
+				returnValues[2] = target.getColor().b;
+				return 3;
+			*/
+			default: assert false; return -1;
+		}
+	}
+
+	@Override
+	public void setValues(JPanel target, int tweenType, float[] newValues) {
+		switch (tweenType) {
+			case POS_XY: target.setLocation((int) newValues[0], (int) newValues[1]); break;
+			case CPOS_XY: target.setLocation((int) newValues[0] - target.getWidth()/2, (int) newValues[1] - target.getHeight()/2); break;
+			/*
+			case SCALE_XY: target.setScale(newValues[0], newValues[1]); break;
+			case ROTATION: target.setRotation(newValues[0]); break;
+			
+			
+			case OPACITY:
+				Color c = target.getColor();
+				c.set(c.r, c.g, c.b, newValues[0]);
+				target.setColor(c);
+				break;
+
+			case TINT:
+				c = target.getColor();
+				c.set(newValues[0], newValues[1], newValues[2], c.a);
+				target.setColor(c);
+				break;
+			*/
+
+			default: assert false;
+		}
+
+	}
+
+}
