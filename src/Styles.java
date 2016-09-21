b4bfece0e5430abe81aa67ec1cb6847c305ce865
Jeremy Ottesen <jeottesen@gmail.com>
2016-09-21 10:26:02 -0600

    Separate Styling Code into it's own class
    Added a Styles class to move button and label styling from Player
    class. This will allow other places to use the same styling for objects
    in other places.
    
    Added antialiasing to smooth card images.

---
+++ src/Styles.java
@@ -0,0 +1,43 @@
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Insets;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.Component;
+import java.awt.Dimension;
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.border.LineBorder;
+
+public class Styles {
+	public static void configureJLabel(JLabel label, boolean isVisible) {
+		label.setForeground(Color.WHITE);
+		label.setFont(new Font("Helvetica", Font.PLAIN, 24));
+		label.setAlignmentX(Component.CENTER_ALIGNMENT);
+		label.setVisible(isVisible);
+	}
+	
+	public static void configureJButton(JButton button, boolean isVisible) {
+		button.setFocusPainted(false);// avoids this ugly inner border
+		button.setOpaque(true);
+		button.setVisible(isVisible);
+		button.setBorder(new LineBorder(Color.WHITE, 2, false));
+		button.setForeground(Color.WHITE);
+		button.setBackground(Globals.BACKGROUND_COLOR_PURPLE);
+		button.setPreferredSize(new Dimension(100, 50));
+		button.setFont(new Font("Helvetica", Font.PLAIN, 22));
+		button.setMargin(new Insets(0, 20, 0, 20));
+		button.addMouseListener(new MouseAdapter() {
+			public void mouseEntered(MouseEvent e) {
+				super.mouseEntered(e);
+				button.setBackground(Globals.BUTTON_HOVER_COLOR_PURPLE);
+			}
+			public void mouseExited(MouseEvent e) {
+				super.mouseExited(e);
+				button.setBackground(Globals.BACKGROUND_COLOR_PURPLE);
+			}
+			
+		});
+	}
+}
