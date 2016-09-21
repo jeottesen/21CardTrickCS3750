b46765424c641f729e01f8e8fea45225ee1f0b3e
amaidie90 <ashleyrichardson@mail.weber.edu>
2016-09-21 10:39:05 -0600

    Splash Completed!!!!!
    Splash completed.

--- src/CardTrick.java
+++ src/CardTrick.java
@@ -1,8 +1,11 @@
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.awt.geom.Rectangle2D;
 import javax.swing.*;
+import javax.swing.border.LineBorder;
 
 
 public class CardTrick extends JFrame
@@ -26,7 +29,7 @@ public class CardTrick extends JFrame
 		add(splash);
 
 		//Add play button
-		addButton(new JButton("Play!"), Globals.FRAME_WI/2, Globals.FRAME_HI/2, 80, 40);
+		addButton(new JButton("Play!"), Globals.FRAME_WI/2, 250, 175, 75);
 		
 		setVisible(true);
 	}
@@ -37,6 +40,27 @@ public class CardTrick extends JFrame
 		//		button.setBackground(Color.CYAN);
 		//		button.setForeground(Color.BLUE);
 		button.setBorderPainted(true);
+		button.setFocusPainted(false);
+		button.setBackground(Globals.BACKGROUND_COLOR_PURPLE);
+		button.setForeground(Color.WHITE);
+		button.setBorder(new LineBorder(Color.WHITE, 2, false));
+		button.setPreferredSize(new Dimension(100, 50));
+		button.setFont(new Font("Helvetica", Font.PLAIN, 36));
+		button.setMargin(new Insets(0, 20, 0, 20));
+		button.addMouseListener(new MouseAdapter() {
+			public void mouseEntered(MouseEvent e) {
+				super.mouseEntered(e);
+				button.setBackground(Globals.BUTTON_HOVER_COLOR_PURPLE);
+				//button.repaint();
+			}
+			public void mouseExited(MouseEvent e) {
+				super.mouseExited(e);
+				//button.setBackground(new Color(255, 255, 255, 0));   //transparent
+				button.setBackground(Globals.BACKGROUND_COLOR_PURPLE);
+				//button.repaint();
+			}
+			
+		});
 
 		//button.addActionListener(l);
 
