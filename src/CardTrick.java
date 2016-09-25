d1c9f5dd8cc9abf528386887211e79ef419ed7b4
amaidie90 <ashleyrichardson@mail.weber.edu>
2016-09-25 16:56:13 -0600

    Font
    Font

--- src/CardTrick.java
+++ src/CardTrick.java
@@ -53,7 +53,7 @@ public class CardTrick extends JFrame {
 	{
 		button.setBounds(locX, locY, Wi, Hi);
 		Styles.configureJButton(button, true);
-		button.setFont(new Font("Helvetica", Font.PLAIN, 36));
+		button.setFont(new Font("Helvetica", Font.BOLD, 36));
 
 		button.addActionListener(new ActionListener() 
 		{
